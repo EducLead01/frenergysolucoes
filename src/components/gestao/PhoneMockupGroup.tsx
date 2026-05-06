5d7de25a2ec415902ade5341800c88a6a7403efa
Cesar Marçal <planejamentoeduclead@gmail.com>
2026-05-05 23:48:22 -0300

    Corrige imagem app03 no mobile: usa width/height explícito ao invés de fill

--- src/components/gestao/PhoneMockupGroup.tsx
+++ src/components/gestao/PhoneMockupGroup.tsx
@@ -3,14 +3,13 @@ import Image from "next/image";
 export function PhoneMockupGroup() {
   return (
     <div className="w-full flex justify-center">
-      <div className="relative w-full max-w-sm h-[280px] lg:h-[340px]">
-        <Image
-          src="/images/app03.png"
-          alt="App Frenergy"
-          fill
-          className="object-contain"
-        />
-      </div>
+      <Image
+        src="/images/app03.png"
+        alt="App Frenergy"
+        width={400}
+        height={320}
+        className="w-full max-w-sm h-auto object-contain"
+      />
     </div>
   );
 }
