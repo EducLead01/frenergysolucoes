6f737b7be4a23b5fb331d93f0d4b42cdc78f8cdb
Cesar Marçal <planejamentoeduclead@gmail.com>
2026-05-18 09:10:26 -0300

    Cases: troca capa VPM Vidros para vpmcapa.png

--- src/components/home/HomeModelos.tsx
+++ src/components/home/HomeModelos.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 
 const cases = [
   { img: "/images/panificadora.png",     href: "/cases/panificadoraparaguai" },
-  { img: "/images/laminex01.png",        href: "/cases/vpmvidros" },
+  { img: "/images/vpmcapa.png",           href: "/cases/vpmvidros" },
   { img: "/images/posto01.png",          href: "/cases/postosabadin" },
 ];
 
